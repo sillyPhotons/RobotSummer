8494b459923ff37c13b1e55aaa5cdabf373a4d5d
sillyPhotons <ruihengsu@gmail.com>
2020-07-08 16:19:26 -0700

    Added correlation code

--- main.cpp
+++ main.cpp
@@ -4,123 +4,89 @@
 #include <NewPing.h>
 #include "main.h"
 #include <vector>
+#include <numeric>
 
-Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
+#define LED_DISPLAY PB12
+// #define PGAIN PA5
+// #define DGAIN PA4
+// #define REFLECTANCE PB1
+#define LED_DISPLAY PB12
+#define BIN_SERVO PA8
+#define ARM_SERVO PA6
+#define IR PA7
 
 // Both TRIG and ECHO are Digital Pins
 #define TRIG PA11
 #define ECHO PA12
-#define MAX_DISTANCE 300
+#define MAX_DISTANCE 50
 
-Servo servo;
+// Servo bin_servo;
+Servo arm_servo;
+
+// Left motor
+#define L_FORWARD PB_8
+#define L_REVERSE PB_9
+
+// Right motor
+#define R_FORWARD PA_1
+#define R_REVERSE PA_0
+
 Motor left_motor = Motor(L_FORWARD, L_REVERSE);
 Motor right_motor = Motor(R_FORWARD, R_REVERSE);
-Phototransistor ir_detection = Phototransistor(IR);
+Phototransistor ir_detection = Phototransistor(IR, 1000);
 NewPing sonar(TRIG, ECHO, MAX_DISTANCE);
-
-void print_to_display(int intensity, int pg, int dg, int error, int speed, double dist);
+Adafruit_SSD1306 display(128, 64, &Wire, -1);
 
 void run2_for_ms(Motor motor1, Motor motor2, int speed1, int speed2, int ms);
 void run1_for_ms(Motor motor1, int speed, int ms);
-void search(void);
+float detect_1KHz(int num_samples);
+bool search(int max_distance, int spin_time, int increments);
+void zero_in();
 
-void setup()
+void zero_in()
 {
-
-    display.begin(SSD1306_SWITCHCAPVCC, 0x3C);
-    display.display();
-    display.setTextColor(SSD1306_WHITE);
-    display.clearDisplay();
-    delay(500);
-    display.setCursor(0, 0);
-    display.print("Welcome, Ray.");
-    display.display();
-    delay(500);
-
-    pinMode(TRIG, OUTPUT);
-    pinMode(ECHO, INPUT);
-    pinMode(LED_DISPLAY, INPUT_PULLUP);
-    // attachInterrupt(digitalPinToInterrupt(LED_DISPLAY),
-    //                 turn_on_display,
-    //                 RISING);
-
-    // servo.attach(SERVO1);
 }
-
-void search(int increments, int ms_per_increment)
+/*
+    @param max_distance: maximum distance in centimeters
+    @param spin_time: time in ms
+*/
+bool search(int max_distance, int spin_time, int increments)
 {
-    int g_i = 0;
-    int g = 100000;
-    std::vector<int> history(increments);
+    // std::vector<int> history(spin_time);
 
-    for (int i = 0; i < increments / 2; i += 1)
-    {
-        run2_for_ms(right_motor, left_motor, 50, -50, ms_per_increment);
-        unsigned long cm = sonar.ping_cm();
-        delay(100);
-        history[i] = cm;
-        if (cm < g)
-        {
-            g = cm;
-            g_i = i;
-        }
-    }
-    for (int i = 0; i < increments / 2; i += 1)
-    {
-        run2_for_ms(right_motor, left_motor, -50, 50, ms_per_increment);
-        delay(100);
-    }
-    // run1_for_ms(right_motor, -50, ms_per_increment* increments/2);
-    for (int i = increments / 2 - 1; i < increments; i += 1)
-    {
-        run2_for_ms(left_motor, right_motor, 50, -50, ms_per_increment);
-        unsigned long cm = sonar.ping_cm();
-        delay(100);
-        history[i] = cm;
-        if (cm < g)
-        {
-            g = cm;
-            g_i = i;
-        }
-    }
-    for (int i = increments / 2 - 1; i < increments; i += 1)
-    {
-        run2_for_ms(left_motor, right_motor, -50, 50, ms_per_increment);
-        delay(100);
-    }
-    // run1_for_ms(left_motor, -50, ms_per_increment * increments/2);
+    int closest = max_distance;
+    int start_tick = HAL_GetTick();
 
-    if (g_i < increments / 2)
-    {   
-        for (int i = 0; i < increments / 2; i += 1)
+    int ms_per_increment = spin_time / increments;
+    while (HAL_GetTick() - start_tick < spin_time)
+    {
+        run2_for_ms(left_motor, right_motor, 40, -40, ms_per_increment);
+        unsigned long cm = sonar.ping_cm();
+        delay(500);
+        if (cm < closest)
         {
-            run2_for_ms(right_motor, left_motor, 50, -50, ms_per_increment);
-            unsigned long cm = sonar.ping_cm();
-            delay(100);
-            int difference = g - cm;
-            if (abs(difference) < 5)
-            {   
-                run2_for_ms(left_motor, right_motor, 70, 70, 1000);
-                return;
-            }
+            closest = cm;
         }
     }
-    
-    if (g_i > increments / 2)
-    {   
-        for (int i = increments / 2 - 1; i < increments; i += 1)
-        {   
-            run2_for_ms(left_motor, right_motor, 50, -50, ms_per_increment);
-            unsigned long cm = sonar.ping_cm();
-            delay(100);   
-            int difference = g - cm;
-            if (abs(difference) < 5)
-            {   
-                run2_for_ms(left_motor, right_motor, 70, 70, 1000);
-                return;
-            }
+
+    if (closest == max_distance)
+    {
+        return false;
+    }
+
+    unsigned long scan = sonar.ping_cm();
+    while (HAL_GetTick() - start_tick < spin_time * 3)
+    {
+        run2_for_ms(left_motor, right_motor, 40, -40, ms_per_increment);
+        unsigned long cm = sonar.ping_cm();
+        delay(500);
+        double difference = 1.0 * cm - 1.0 * closest;
+        if (abs(difference) / (1.0 * cm + 1.0 * closest) / 2.0 < 0.1)
+        {
+            return true;
         }
     }
+    return false;
 }
 
 void run2_for_ms(Motor motor1, Motor motor2, int speed1, int speed2, int ms)
@@ -133,6 +99,7 @@ void run2_for_ms(Motor motor1, Motor motor2, int speed1, int speed2, int ms)
     }
     motor2.run_motor(0);
     motor1.run_motor(0);
+    return;
 }
 
 void run1_for_ms(Motor motor, int speed, int ms)
@@ -143,59 +110,110 @@ void run1_for_ms(Motor motor, int speed, int ms)
         motor.run_motor(speed);
     }
     motor.run_motor(0);
+    return;
 }
-// void print_to_display(int intensity, int pg, int dg, int error, int speed, double dist)
-// {
-//     display.clearDisplay();
-//     display.setCursor(0, 0);
-//     display.print("Sonar: ");
-//     display.println(dist);
-//     display.print("Intensity: ");
-//     display.println(intensity);
-//     display.display();
-//     display.print("PGain: ");
-//     display.println(pg);
-//     display.print("DGain: ");
-//     display.println(dg);
-//     display.print("Error: ");
-//     display.println(error);
-//     display.print("Speed: ");
-//     display.println(speed);
-//     display.display();
-//     delay(100);
-// }
+
+float detect_1KHz(int num_samples)
+{
+    float raw[num_samples] = {0};
+
+    int start = HAL_GetTick();
+    for (int i = 0; i < num_samples; i += 1)
+    {   
+        raw[i] = analogRead(PA7);
+        delay(1);
+    }
+    int ms_taken = HAL_GetTick() - start;
+    double sampling_period = ms_taken / (num_samples * 1000.0);
+    int sampling_hz = num_samples / sampling_period; // in Hz
+
+    float ref_freq = 1000.0;
+
+    float cosine_ref[num_samples] = {0};
+    float sine_ref[num_samples] = {0};
+
+    for (int i = 0; i < num_samples; i += 1)
+    {
+        cosine_ref[i] = cos(i * 2 * PI * ref_freq / sampling_hz);
+        sine_ref[i] = sin(i * 2 * PI * ref_freq / sampling_hz);
+    }
+
+    float real = std::inner_product(raw, raw + sizeof(raw) / sizeof(raw[0]), cosine_ref, 0);
+    float imaginary = std::inner_product(raw, raw + sizeof(raw) / sizeof(raw[0]), sine_ref, 0);
+    double frequency_intensity = sqrt((real * real) + (imaginary * imaginary));
+
+    return frequency_intensity;
+}
+
+void setup()
+{
+    // Set up display
+    pinMode(LED_DISPLAY, INPUT_PULLUP);
+    display.begin(SSD1306_SWITCHCAPVCC, 0x3C);
+    display.display();
+    display.setTextColor(SSD1306_WHITE);
+    display.clearDisplay();
+    display.setCursor(0, 0);
+    delay(1000);
+
+    pinMode(TRIG, OUTPUT);
+    pinMode(ECHO, INPUT);
+
+    // attachInterrupt(digitalPinToInterrupt(LED_DISPLAY),
+    //                 turn_on_display,
+    //                 RISING);
+
+    // bin_servo.attach(BIN_SERVO);
+    arm_servo.attach(ARM_SERVO);
+}
 
 int test_cm = 30;
-
+int max_distance = MAX_DISTANCE;
 void loop()
 {
-    // int intensity = analogRead(ir_detection.get_pin());
-    // display.clearDisplay();
-    // display.setCursor(0, 0);
-    // display.print(intensity);
-    // delay(100);
-    search(20, 50);
-    return; 
+    // run2_for_ms(right_motor, left_motor, 100, 100, 1000);
+    // pwm_start(PA_6, 50, 2000, MICROSEC_COMPARE_FORMAT);
+    // run2_for_ms(right_motor, left_motor, 50, 50, 500);
+    // pwm_start(PA_6, 50, 1250, MICROSEC_COMPARE_FORMAT);
+    // delay(5000);
+    // pwm_start(PA_6, 50, 2550, MICROSEC_COMPARE_FORMAT);
+    // delay(10000);
 
-    int current_tick = HAL_GetTick();
+    float intensity = detect_1KHz(100);
+    display.clearDisplay();
+    display.setCursor(0, 0);
+    display.print(intensity);
+    display.display();
 
-    if (current_tick < TOTAL_TIME - HOMING_TIME)
-    {   
-        
-        // left_motor.run_motor(50);
+    // bool found = search(max_distance, 3000, 20);
+    // if (found)
+    // {
+    //     run2_for_ms(right_motor, left_motor, 100, 100, 500);
+    // }
+    // else
+    // {
+    //     max_distance += 10;
+    // }
 
-        // if (cm > test_cm)
-        // {
-        // }
-        // else if (cm < test_cm)
-        // {
-        //     right_motor.run_motor(-20);
-        // }
-        // else
-        // {
-        //     right_motor.run_motor(0);
-        // }
-    }
+    // int current_tick = HAL_GetTick();
+
+    // if (current_tick < TOTAL_TIME - HOMING_TIME)
+    // {
+
+    // left_motor.run_motor(50);
+
+    // if (cm > test_cm)
+    // {
+    // }
+    // else if (cm < test_cm)
+    // {
+    //     right_motor.run_motor(-20);
+    // }
+    // else
+    // {
+    //     right_motor.run_motor(0);
+    // }
+    // }
     // int pg = analogRead(PGAIN) / 10;
     // int dg = analogRead(DGAIN);
     // // int dg = 0;
@@ -229,9 +247,9 @@ void loop()
 
     // last_error = error;
     // }
-    else
-    {
-        left_motor.run_motor(0);
-        right_motor.run_motor(0);
-    }
+    // else
+    // {
+    //     left_motor.run_motor(0);
+    //     right_motor.run_motor(0);
+    // }
 };
--- main.h
+++ main.h
@@ -1,27 +1,3 @@
-/**
- * Header File for Main
- **/
-#define SCREEN_WIDTH 128 // OLED display width, in pixels
-#define SCREEN_HEIGHT 64 // OLED display height, in pixels
-#define OLED_RESET -1    // This display does not have a reset pin accessible
-
-#define LED_DISPLAY PB12
-
-#define PGAIN PA5
-#define DGAIN PA4
-#define REFLECTANCE PB1
-#define LED_DISPLAY PB12
-#define SERVO1 PB0
-#define IR PA7
-
-// Left motor
-#define L_FORWARD PB_8
-#define L_REVERSE PB_9
-
-// Right motor
-#define R_FORWARD PA_1
-#define R_REVERSE PA_0
-
 // Timing variables
 const int TOTAL_TIME = 60000;
 int HOMING_TIME = 10000;
@@ -31,6 +7,16 @@ const int SETPOINT = 250;
 const int GAIN = 5;
 int last_error = 0;
 
+class Arm
+{
+public:
+    Arm(PinName arm_servo): pin(arm_servo){};
+    void up_to_down(){
+        pwm_start(PA_6, 50, 1000, MICROSEC_COMPARE_FORMAT);
+    }
+private:
+    PinName pin;
+};
 class Motor
 {
 public:
@@ -46,7 +32,7 @@ public:
             speed = map(speed, 0, 100, 780, 1023);
             pwm_start(forward_pin, PWM_FREQUENCY * 5, speed, RESOLUTION_10B_COMPARE_FORMAT);
             pwm_start(reverse_pin, PWM_FREQUENCY * 5, 0, RESOLUTION_10B_COMPARE_FORMAT);
-        }
+        } 
         else if (speed < 0)
         {
             speed = map(speed, -100, 0, -1023, -780);
@@ -68,8 +54,13 @@ private:
 class Phototransistor
 {
 public:
-    Phototransistor(int photo_pin) : photo_pin(photo_pin){};
-    
+
+    Phototransistor(int photo_pin, double target_frequency) : photo_pin(photo_pin), target_frequency(target_frequency)
+    {
+        
+        ;
+    };
+
     int get_pin()
     {
         return photo_pin;
@@ -77,4 +68,5 @@ public:
 
 private:
     int photo_pin = 0;
+    double target_frequency;
 };
